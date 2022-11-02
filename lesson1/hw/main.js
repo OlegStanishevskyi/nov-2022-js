617d153650651d070139624d5774f4774980ca83
BlackHead <olegvolodimirovic56@gmail.com>
2022-11-02 14:46:20 +0200

    do hw 1 js

---
+++ lesson1/hw/main.js
@@ -0,0 +1,51 @@
+// Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+// Вивести кожну змінну за допомогою: console.log
+
+
+let greeting = 'hello'
+console.log(greeting)
+let name ='owu'
+console.log(name)
+let domen ='com'
+console.log(domen)
+let ua ='country'
+console.log(ua)
+let number1 =1
+console.log(number1)
+let number10 =10
+console.log(number10)
+let numberminus999 = -999
+console.log(numberminus999)
+let number123 = 123
+console.log(number123)
+let pi =3.14
+console. log(pi)
+let number2point7 = 2.7
+console.log(number2point7)
+let number16 = 16
+console.log(number16)
+let bool1 = 'true'
+console.log(bool1);
+let bool2 = 'false'
+console.log(bool2);
+
+// Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+
+let firstName = 'Oleg'
+let middleName = 'Volodymyrovych'
+let lastName = 'Stanishevskyi'
+let person = firstName + ' ' +  lastName + ' ' + middleName
+console.log(person);
+
+// let person = `${firstName} ${middleName} ${lastName}`;
+// console.log(person);
+
+// За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//     let a = 100; let b = '100'; let c = true;
+
+let a = 100
+console.log(typeof a)
+let b = '100'
+console.log(typeof b)
+let c = true
+console.log(typeof c)
